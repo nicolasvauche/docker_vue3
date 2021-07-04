19deb653c917de496bd93df44e443332016e1b13
Nicolas vauché <hello@nicolasvauche.net>
2021-07-04 21:36:31 +0200

    Fetch SF5 API

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <header>
+      <h1>Vue3 API Frontend</h1>
+    </header>
+    <Post />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Post from "@/components/Post";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Post,
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+  color: #ffffff;
+  background-color: #2c3e50;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+section {
+  padding: 2rem 1.5rem;
 }
 </style>
---
+++ app/src/components/Post.vue
@@ -0,0 +1,135 @@
+<template>
+  <section class="posts">
+    <h2>Posts from SF5 API:</h2>
+
+    <p v-if="loading">Still loading..</p>
+    <p v-if="error">{{ error }}</p>
+
+    <div class="thumbnails" v-if="!loading && data && data.length">
+      <div class="thumbnail" v-for="post of data" :key="post.id">
+        <figure class="thumbnail-image">
+          <img :src="post.image" :alt="post.title" />
+        </figure>
+        <h3 class="thumbnail-title">{{ post.title }}</h3>
+        <p class="thumbnail-excerpt">
+          {{
+            post.content.length > 75
+              ? post.content.substring(0, 75) + "…"
+              : post.content
+          }}
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+export default {
+  name: "Posts",
+  props: {},
+  setup() {
+    const data = ref(null);
+    const loading = ref(true);
+    const error = ref(null);
+
+    function fetchData() {
+      loading.value = true;
+      // I prefer to use fetch
+      // you can use use axios as an alternative
+      return fetch("http://localhost:8088/api", {
+        method: "get",
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+        .then((res) => {
+          // a non-200 response code
+          if (!res.ok) {
+            // create error instance with HTTP status text
+            const error = new Error(res.statusText);
+            error.json = res.json();
+            throw error;
+          }
+
+          return res.json();
+        })
+        .then((json) => {
+          // set the response data
+          data.value = json;
+        })
+        .catch((err) => {
+          error.value = err;
+          // In case a custom JSON error response was provided
+          if (err.json) {
+            return err.json.then((json) => {
+              // set the JSON response message
+              error.value.message = json.message;
+            });
+          }
+        })
+        .then(() => {
+          loading.value = false;
+        });
+    }
+
+    onMounted(() => {
+      fetchData();
+    });
+
+    return {
+      data,
+      loading,
+      error,
+    };
+  },
+};
+</script>
+
+<style>
+.thumbnails {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 1.5rem;
+}
+
+.thumbnail {
+  border: 1px solid #cfcfcf;
+  border-radius: 0.5rem;
+  color: #2c3e50;
+  background-color: #ffffff;
+}
+
+.thumbnail-image {
+  margin: 0;
+  height: 150px;
+  overflow: hidden;
+  border-top-left-radius: 0.5rem;
+  border-top-right-radius: 0.5rem;
+}
+
+.thumbnail-image img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-top-left-radius: 0.5rem;
+  border-top-right-radius: 0.5rem;
+  transform: scale(110%);
+  opacity: 0.85;
+  transition: all 0.6s;
+}
+
+.thumbnail:hover .thumbnail-image img {
+  transform: scale(100%);
+  opacity: 1;
+}
+
+.thumbnail-title {
+  padding: 0 0.5rem;
+}
+
+.thumbnail-excerpt {
+  padding: 0 0.5rem;
+}
+</style>
